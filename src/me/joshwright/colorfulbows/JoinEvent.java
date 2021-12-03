99742cd6d37412fc3acf55061f109df8747e744c
Josh Wright <joshnotwright@gmail.com>
2021-12-03 17:36:13 -0500

    fix: update resource pack link

--- src/me/joshwright/colorfulbows/JoinEvent.java
+++ src/me/joshwright/colorfulbows/JoinEvent.java
@@ -8,6 +8,6 @@ public class JoinEvent implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        event.getPlayer().setResourcePack("https://github.com/JoshNotWright/ColorfulBows/releases/download/v0.4.0-alpha/Colorful.Bows.Resources.zip"); // Direct download to plugin resource pack
+        event.getPlayer().setResourcePack("https://github.com/JoshNotWright/ColorfulBows/releases/download/v1.0/Colorful.Bows.Resources.zip"); // Direct download to plugin resource pack
     }
 }
